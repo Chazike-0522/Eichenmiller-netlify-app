7c9d2b1f9f227486c1eca1d475cac8a3da0829a6
unknown <aleeic0522@students.ecpi.edu>
2025-02-26 13:12:43 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import Pic1 from "./download.jpg";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Pic1} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
